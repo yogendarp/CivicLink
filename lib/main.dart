2f8701e51171d4745219425ae98d44a621f61367
yogendarp <yogendarp11@gmail.com>
2026-08-17 15:25:51 +0530

    feat: integrate Firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ import 'package:flutter/material.dart';
 import 'screens/auth/welcome_screen.dart';
 import 'screens/auth/login_screen.dart';
 import 'screens/auth/register_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const CivicLinkApp());
 }
 
--- lib/screens/auth/welcome_screen.dart
+++ lib/screens/auth/welcome_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'register_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
